7574bd62ac0feab404d74663e69e1633a400cf3b
DarleyDev <silotdarley@gmail.com>
2026-05-12 17:46:21 -0400

    add comment to os.path.expanduser()

--- Source/api/core/gradle_caches.py
+++ Source/api/core/gradle_caches.py
@@ -3,8 +3,8 @@ import shutil
 
 
 def removing(cache_path):
-    full_path_cache = os.path.expanduser(cache_path)
-
+    full_path_cache = os.path.expanduser(cache_path) #This line helps to access to the full path of the caches.
+    
     if os.path.exists(full_path_cache):
         shutil.rmtree(full_path_cache)
         print('cache removed')
