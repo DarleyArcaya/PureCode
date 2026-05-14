c34aee9947bff53d036fa070ca46531933c12959
DarleyDev <silotdarley@gmail.com>
2026-05-13 20:46:37 -0400

    Added onexc parameter to intercept errors and fix file permission during deletion

--- Source/api/core/flutter_caches.py
+++ Source/api/core/flutter_caches.py
@@ -1,5 +1,10 @@
 import os
 import shutil
+import stat
+
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
 
 def clear_flutter_cache():
     
@@ -9,12 +14,14 @@ def clear_flutter_cache():
     # getting flutter cache path for linux and macos
     linux_mac_cache_path = os.path.expanduser('~/.pub-cache')
 
-    if os.path.exists(windows_cache_path):
-        shutil.rmtree(windows_cache_path)
-        return 'Flutter caches removed successfully'
-    elif os.path.exists(linux_mac_cache_path):
-        shutil.rmtree(linux_mac_cache_path)
-        return 'Flutter caches removed successfully'
-    else:
-        return 'No cache found'
-    
+    try:
+        if os.path.exists(windows_cache_path):
+            shutil.rmtree(windows_cache_path, onexc=remove_readonly) # using onexc parameter to intercept errors and fix file permissions during deletion
+            return 'Flutter caches removed successfully'
+        elif os.path.exists(linux_mac_cache_path):
+            shutil.rmtree(linux_mac_cache_path, onexc=remove_readonly)
+            return 'Flutter caches removed successfully'
+        else:
+            return 'No cache found'
+    except Exception:
+        return 'An error occurred while trying to remove flutter caches'
