1fa3b60a7d21af1095afaa86ad56af9bfa31a128
Darley Omar Silot Arcaya <silotdarley@gmail.com>
2026-05-17 14:26:11 -0400

    added: New updated in UI: -Changed simple text for TextField with read only activated. -New background color added, now is white the background and the border radious is gray. -New padding and border radious added on the appbar, here it is where we will explaing what the program does.

--- Source/client/lib/homepage.dart
+++ Source/client/lib/homepage.dart
@@ -11,7 +11,7 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -19,19 +19,72 @@ class _HomepageState extends State<Homepage> {
             Padding(
               padding: EdgeInsets.all(20.0),
               child: Container(
+                height: 100,
+                width: 800,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.grey[200],
+              
+                ),
+                child: Text('Here, you will read what the program does'),
+                
+              ),
+
+              
+              ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Container(
                 width: 800,
                 height: 400,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.grey[850],
+                  color: Colors.grey[200],
                   ),
                   child: const Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Android Caches'),
-                      Text('Flutter Caches'),
-                      Text('Gradle Caches'),
-                      Text('PiP Caches')
+                      Text('What caches will be automaticly cleaned'),
+                      SizedBox(height: 30),
+                      
+                      TextField(
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'flutter caches'
+                        ),
+                      ),
+
+                      SizedBox(height: 30),
+
+                      TextField(
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Android Caches'
+                        ),
+                      ),
+
+                      SizedBox(height: 30),
+
+                      TextField(
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Gradle Caches'
+                        ),  
+                      ),
+
+                      SizedBox(height: 30),
+
+                      TextField(
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'PIP Caches'
+                        ),
+                      )
+
                       ],
                   ),
                   ),
