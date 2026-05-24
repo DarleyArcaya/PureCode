af3c924d578e3a4adc864e7cf681bc3d55f4d6e0
Darley Omar Silot Arcaya <silotdarley@gmail.com>
2026-05-23 22:39:51 -0400

    Feat: Start working on separated pick clean cache file

--- Source/client/lib/api_services.dart
+++ Source/client/lib/api_services.dart
@@ -13,10 +13,20 @@ class ApiServices {
     
     try {
 
-      // Performs an asynchronous GET request to the unified cleanup endpoint
+      // Performs an asynchronous GET request to cleanup endpoint
       final response = await http.get(Uri.parse('http://127.0.0.1:8000/clean_all_caches'));
+      final gradleCaches = await http.get(Uri.parse('http://127.0.0.1:8000/gradle_caches'));
+      final androidCaches = await http.get(Uri.parse('http://127.0.0.1:8000/android_caches'));
+      final flutterCaches = await http.get(Uri.parse('http://127.0.0.1:8000/flutter_caches'));
+      final pipCaches = await http.get(Uri.parse('http://127.0.0.1:8000/pip_caches'));
+
       // Checks if the HTTP status code is exactly 200 (OK)
-      return response.statusCode == 200;
+      return response.statusCode == 200 &&
+              gradleCaches.statusCode == 200 &&
+              androidCaches.statusCode == 200 &&
+              flutterCaches.statusCode == 200 &&
+              pipCaches.statusCode == 200;
+
       
     } catch (e) {
       // Catches any connection errors, timeouts, or server down issues securely
