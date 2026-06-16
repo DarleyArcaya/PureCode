732b778e14c865973f44b9bdee0ea3127d83d06f
DarleyDev <silotdarley@gmail.com>
2026-06-16 12:25:52 -0400

    feat: testing new function for optimization

--- Source/api/tests/optimization_sys.py
+++ Source/api/tests/optimization_sys.py
@@ -1,14 +1,2 @@
-import platform
+import platform, os, sys
 
-def optimization_system():
-
-    os = platform.system()
-    if os == "Windows":
-        print("This system is window")
-    elif os == "Darwin":
-        print("This system is Darwin")
-    else:
-        print("This system is other")
-    
-
-optimization_system()
