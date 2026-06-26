9738b4121a77f6f3086e219cbe35894aa35a014e
DarleyDev <silotdarley@gmail.com>
2026-06-25 21:48:34 -0400

    added info.txt for remember how to use the folder test
    there is important info about pytest and how to call functions in tests

---
+++ Source/api/tests/test_ensure_admin.py
@@ -0,0 +1,8 @@
+# import pytest
+# use asstert once I call function from core
+
+import pytest
+from core.optimosys import ensure_admin
+
+def test_ensure_admin():
+    assert ensure_admin()
--- Source/api/tests/test_is_admin.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-# use asstert once I call function from core
-
-import pytest
-from core.optimosys import is_admin
-
-def test_is_admin():
-    assert is_admin()
