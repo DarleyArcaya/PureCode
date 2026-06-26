35896161f26d19295919a4472a1546337490117b
DarleyDev <silotdarley@gmail.com>
2026-06-25 22:06:16 -0400

    Trying to understand how to use pytest....

--- Source/api/tests/test_ensure_admin.py
+++ Source/api/tests/test_ensure_admin.py
@@ -2,7 +2,7 @@
 # use asstert once I call function from core
 
 import pytest
-from core.optimosys import ensure_admin
+from core.optimosys.optimosys import ensure_admin
 
 def test_ensure_admin():
     assert ensure_admin()
