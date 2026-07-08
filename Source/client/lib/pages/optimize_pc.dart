b70fff2655dd4d02a5d9315907f3f39fcf722df3
DarleyDev <silotdarley@gmail.com>
2026-07-07 20:49:44 -0400

    Working on last feature about show what will be optimize en showdialog

--- Source/client/lib/pages/optimize_pc.dart
+++ Source/client/lib/pages/optimize_pc.dart
@@ -136,7 +136,7 @@ class _MyWidgetState extends State<OptimizeWindow> {
                           builder: (BuildContext context) {
                             return AlertDialog(
                               title: const Text('Optimization Process', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-                              content: Text('You may save: ', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                              content: Text('You may save: $textgbSaved', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
                               actions: [
                                 TextButton(
                                   onPressed: () {
